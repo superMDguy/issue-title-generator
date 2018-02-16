c0243cfddc08d308a5e4a68509ca61a759c9355b
Matthew Dangerfield <matthew.dangerfield@yandex.com>
2018-02-16 08:20:16 -0600

    multiprocessing

--- issues_problem/issues_problem.py
+++ issues_problem/issues_problem.py
@@ -1,64 +1,76 @@
 from os import path
+import gc
+import pickle
+from multiprocessing import Pool
 
 from tqdm import tqdm
 import pandas as pd
 from tensor2tensor.data_generators import problem, generator_utils, text_encoder
 from tensor2tensor.utils import registry
-
-EOS = text_encoder.EOS_ID
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
 
 split_token = ' <$issue_body$> '
-ISSUES_FILE = path.expanduser('~/Code/dl/datasets/github_issues.csv'), chunksize=chunksize
+EOS = text_encoder.EOS_ID
+ISSUES_FILE = path.expanduser('~/Code/dl/datasets/github_issues.csv')
+VOCAB_FILE = path.join(data_dir, 'vocab_encoder.pkl')
 
-def issue_title_generator(chunksize=10000):
+encoder = None
+try:
+    encoder = pickle.load(open(VOCAB_FILE, 'rb'))
+    print('Loaded saved vocab file from', VOCAB_FILE)
+except Exception:
+    encoder = LabelEncoder()
+    print('Couldn\'t find saved vocab file, starting from scratch')
+
+def get_data():
     print('Reading csv...')
-    issues_batches = pd.read_csv(ISSUES_FILE)
-    for issues in tqdm(issues_batches, total=(5000000 // chunksize)):
-        concatenated = list(issues.issue_title + split_token + issues.body)
-        for row in concatenated:
-            yield row
+    issues = pd.read_csv(ISSUES_FILE)
+    return list(issues.issue_title + split_token + issues.body)
 
+def encode(text):
+    encoded = encoder.transform(list(text)) + 2 # 0 and 1 are reserved by t2t
+    encoded = encoded.tolist()
+    encoded.append(EOS)
+    return encoded
+        
+def encode_row(row):
+    title, body = row.split(split_token)
+    return {"inputs": encode(body), "targets": encode(title)}
 
 @registry.register_problem
 class IssueToTitle(problem.Text2TextProblem):
     @property
     def is_character_level(self):
-        return False
-
-    @property
-    def targeted_vocab_size(self):
-        return 20000
+        return True
 
     @property
     def vocab_name(self):
         return "vocab.gh_issue"
 
     @property
-    def input_space_id(self):
-        return problem.SpaceID.EN_TOK
-
-    @property
-    def target_space_id(self):
-        return problem.SpaceID.EN_TOK
-
-    @property
     def num_shards(self):
-        return 100
+        return 10
 
-    @property
-    def use_subword_tokenizer(self):
-        return True
+    def train_encoder(self, data):
+        print('Running label encoder...')
+        concat_text = ' '.join(data)
+
+        uniq_chars = set()
+        for char in concat_text: uniq_chars.add(char)
+
+        del concat_text
+        for i in range(3): gc.collect()
+
+        encoder.fit(list(uniq_chars))
+        pickle.dump(encoder, open(VOCAB_FILE, 'wb'))
 
     def generator(self, data_dir, tmp_dir, is_training):
-        encoder = generator_utils.get_or_generate_vocab_inner(
-            data_dir, self.vocab_file, self.targeted_vocab_size,
-            issue_title_generator())
+        data = get_data()
 
-        print('done encoding')
-        for issue in issue_title_generator():
-            import pdb; pdb.set_trace()
-            title, body = issue.split(split_token)
-            encoded_title = encoder.encode(title) + [EOS]
-            encoded_body = encoder.encode(body) + [EOS]
-            yield {"inputs": encoded_body, "targets": encoded_title}
+        if not hasattr(encoder, 'classes_'):
+            train_encoder(data)
+
+        with Pool() as p:
+            return tqdm(p.imap(encode_row, data), total=len(data))
 
